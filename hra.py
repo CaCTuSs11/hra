ef853e98665cf72a75b5451646b697d36a6ab5fe
CaC_TuSs <viktorkopper111@gmail.com>
2020-06-09 00:04:51 +0200

    obrazky a zaciatok objektoveho prg

--- hra.py
+++ hra.py
@@ -2,23 +2,57 @@ import pygame
 
 pygame.init()
 
-win = pygame.display.set_mode((500, 500))
+win = pygame.display.set_mode((500, 480))
 pygame.display.set_caption("First_Game")
 screenWidth = 500
-screenHeight = 500
+screenHeight = 480
 
-x = 50
-y = 450
-width = 40
-height = 60
-vel = 10  # postavičaka
+walkRight = [pygame.image.load('R1.png'), pygame.image.load('R2.png'), pygame.image.load('R3.png'),
+             pygame.image.load('R4.png'), pygame.image.load('R5.png'), pygame.image.load('R6.png'),
+             pygame.image.load('R7.png'), pygame.image.load('R8.png'), pygame.image.load('R9.png')]
+walkLeft = [pygame.image.load('L1.png'), pygame.image.load('L2.png'), pygame.image.load('L3.png'),
+            pygame.image.load('L4.png'), pygame.image.load('L5.png'), pygame.image.load('L6.png'),
+            pygame.image.load('L7.png'), pygame.image.load('L8.png'), pygame.image.load('L9.png')]
+bg = pygame.image.load('bg.jpg')
+char = pygame.image.load('standing.png')
 
-isJump = False
-jumpCount = 10
+clock = pygame.time.Clock()
+
+
+class player(object):
+    def __init__(self, x, y, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.vel = 5
+        self.isJump = False
+        self.jumpCount = 10
+        self.left = False
+        self.right = False
+        self.walkCount = 0
+
+
+def redrawGameWindow():
+    global walkCount
+    win.blit(bg, (0, 0))  # pozadie
+    if walkCount + 1 >= 27:
+        walkCount = 0
+
+    if left:
+        win.blit(walkLeft[walkCount // 3], (x, y))
+        walkCount += 1
+    elif right:
+        win.blit(walkRight[walkCount // 3], (x, y))
+        walkCount += 1
+    else:
+        win.blit(char, (x, y))
+    pygame.display.update()
+
 
 run = True
 while run:
-    pygame.time.delay(30)  # pustena hra
+    clock.tick(27)  # pustena hra
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -27,11 +61,23 @@ while run:
     keys = pygame.key.get_pressed()  # drzanie klaves
     if keys[pygame.K_LEFT] and x > vel:
         x -= vel
-    if keys[pygame.K_RIGHT] and x < screenWidth - width - vel:
+        left = True
+        right = False
+    elif keys[pygame.K_RIGHT] and x < screenWidth - width - vel:
         x += vel
+        right = True
+        left = False
+    else:
+        right = False
+        left = False
+        walkCount = 0
+
     if not isJump:
         if keys[pygame.K_SPACE]:
             isJump = True
+            right = False
+            left = False
+            walkCount = 0
     else:
         if jumpCount > 0:
             y -= (jumpCount ** 2)
@@ -42,8 +88,8 @@ while run:
         else:
             isJump = False
             jumpCount = 10
-    win.fill((0, 0, 0))  # nerobi farebnu ciaru
-    pygame.draw.rect(win, (255, 0, 0), (x, y, width, height))
-    pygame.display.update()
+    redrawGameWindow()
 
 pygame.quit()
+# alt+enter= chyba
+# alt+ctrl+l=formatovanie
