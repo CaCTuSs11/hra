f066f61643296a8a38bbd27caf89274a1159b869
CaC_TuSs <viktorkopper111@gmail.com>
2020-06-08 22:36:19 +0200

    font

--- hra.py
+++ hra.py
@@ -1,53 +1,49 @@
 import pygame
+
 pygame.init()
 
-win=pygame.display.set_mode((500,500))
+win = pygame.display.set_mode((500, 500))
 pygame.display.set_caption("First_Game")
-screenWidth=500
-screenHeight=500
+screenWidth = 500
+screenHeight = 500
 
+x = 50
+y = 450
+width = 40
+height = 60
+vel = 10  # postavičaka
 
-x= 50
-y= 450
-width=40
-height=60
-vel=10           #postavičaka
+isJump = False
+jumpCount = 10
 
-
-isJump= False
-jumpCount=10
-
-
-
-
-run= True
+run = True
 while run:
-    pygame.time.delay(30) #pustena hra
+    pygame.time.delay(30)  # pustena hra
 
     for event in pygame.event.get():
-        if event.type== pygame.QUIT:
-            run= False          #vypnutie hry
+        if event.type == pygame.QUIT:
+            run = False  # vypnutie hry
 
-    keys=pygame.key.get_pressed()#drzanie klaves
-    if keys[pygame.K_LEFT]and x>vel:
-        x-= vel
-    if keys[pygame.K_RIGHT]and x<screenWidth-width-vel:
-        x+=vel
-    if not(isJump):     
+    keys = pygame.key.get_pressed()  # drzanie klaves
+    if keys[pygame.K_LEFT] and x > vel:
+        x -= vel
+    if keys[pygame.K_RIGHT] and x < screenWidth - width - vel:
+        x += vel
+    if not isJump:
         if keys[pygame.K_SPACE]:
             isJump = True
     else:
         if jumpCount > 0:
-            y -= (jumpCount **2)
-            jumpCount -=1
-        elif jumpCount>=-10:
-            y -= -(jumpCount **2)
-            jumpCount -=1
+            y -= (jumpCount ** 2)
+            jumpCount -= 1
+        elif jumpCount >= -10:
+            y -= -(jumpCount ** 2)
+            jumpCount -= 1
         else:
-            isJump= False
-            jumpCount= 10
-    win.fill((0,0,0))#nerobi farebnu ciaru
-    pygame.draw.rect(win, (255,0,0),(x,y,width,height))
+            isJump = False
+            jumpCount = 10
+    win.fill((0, 0, 0))  # nerobi farebnu ciaru
+    pygame.draw.rect(win, (255, 0, 0), (x, y, width, height))
     pygame.display.update()
-    
+
 pygame.quit()
